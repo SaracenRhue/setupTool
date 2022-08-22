efd136e3b7464476ab63964fc8479369e4dfdefd
SaracenRhue <77064702+SaracenRhue@users.noreply.github.com>
2022-08-22 18:55:59 +0200

    fix

--- desktops/fedora/install_packages.sh
+++ desktops/fedora/install_packages.sh
@@ -7,8 +7,8 @@ sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 printf "[vscode]\nname=packages.microsoft.com\nbaseurl=https://packages.microsoft.com/yumrepos/vscode/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc\nmetadata_expire=1h" | sudo tee -a /etc/yum.repos.d/vscode.repo
 
 
-sudo apt update -y
-sudo apt upgrade -y
+sudo dnf update -y
+sudo dnf upgrade -y
     
 
 PKGS=(
