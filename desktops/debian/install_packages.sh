26790a9a35c541956ca93cbe903ccc5d52d15761
SaracenRhue <77064702+SaracenRhue@users.noreply.github.com>
2022-09-06 19:52:32 +0200

    added nix

--- desktops/debian/install_packages.sh
+++ desktops/debian/install_packages.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-sudo apt install software-properties-common apt-transport-https -y
-    sudo curl -sSL https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
-    sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
-    sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-    echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-    echo "deb [arch=amd64,arm64,armhf] http://deb.volian.org/volian/ scar main" | sudo tee /etc/apt/sources.list.d/volian-archive-scar-unstable.list
-    wget -qO - https://deb.volian.org/volian/scar.key | sudo tee /etc/apt/trusted.gpg.d/volian-archive-scar-unstable.gpg > /dev/null
+#sudo apt install software-properties-common apt-transport-https -y
+#sudo curl -sSL https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+#sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+#sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+#echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+#echo "deb [arch=amd64,arm64,armhf] http://deb.volian.org/volian/ scar main" | sudo tee /etc/apt/sources.list.d/volian-archive-scar-unstable.list
+#wget -qO - https://deb.volian.org/volian/scar.key | sudo tee /etc/apt/trusted.gpg.d/volian-archive-scar-unstable.gpg > /dev/null
 
     sudo apt update -y
     sudo apt upgrade -y
@@ -27,8 +27,8 @@ PKGS=(
 'nodejs'
 'npm'
 'krusader'
-'brave-browser'
-'code'
+#'brave-browser'
+#'code'
 'openjdk-17-jdk'
 'papirus-icon-theme'
 )
--- desktops/fedora/install_packages.sh
+++ desktops/fedora/install_packages.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/
-sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
+#sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/
+#sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
 
-sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
-printf "[vscode]\nname=packages.microsoft.com\nbaseurl=https://packages.microsoft.com/yumrepos/vscode/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc\nmetadata_expire=1h" | sudo tee -a /etc/yum.repos.d/vscode.repo
+#sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+#printf "[vscode]\nname=packages.microsoft.com\nbaseurl=https://packages.microsoft.com/yumrepos/vscode/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc\nmetadata_expire=1h" | sudo tee -a /etc/yum.repos.d/vscode.repo
 
 
 sudo dnf update -y
@@ -13,7 +13,7 @@ sudo dnf upgrade -y
 
 PKGS=(
 'arc'
-'code'
+#'code'
 'tor'
 'gparted'
 'htop'
@@ -26,7 +26,7 @@ PKGS=(
 'nodejs'
 'npm'
 'krusader'
-'brave-browser'
+#'brave-browser'
 'openjdk-17-jdk'
 'papirus-icon-theme'
 )
