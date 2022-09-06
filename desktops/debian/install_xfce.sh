16c2dd372b5a26459cff29b5e40bd9cfeea141d8
SaracenRhue <77064702+SaracenRhue@users.noreply.github.com>
2022-09-06 14:17:47 +0200

    fix

--- desktops/debian/install_xfce.sh
+++ desktops/debian/install_xfce.sh
@@ -3,3 +3,4 @@
 sudo apt install -y xfce4 
 sudo systemctl enable lightdm
 sudo systemctl set-default graphical.target
+sudo apt install -y firefox-esr
--- desktops/fedora/install_xfce.sh
+++ desktops/fedora/install_xfce.sh
@@ -3,3 +3,4 @@
 sudo dnf install @xfce-desktop-environment -y
 sudo systemctl enable lightdm
 sudo systemctl set-default graphical.target
+sudo dnf install firefox -y
