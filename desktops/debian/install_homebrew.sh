63156eb5f4dd37c9b584783c8bc16f8bdf3636ee
SaracenRhue <77064702+SaracenRhue@users.noreply.github.com>
2022-09-20 12:11:42 +0200

    Update install_homebrew.sh

--- desktops/debian/install_homebrew.sh
+++ desktops/debian/install_homebrew.sh
@@ -2,7 +2,7 @@
 
 sudo apt install -y build-essential procps file
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+yes '' | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> ~/.bashrc
 echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> ~/.zshrc
