ef60d58eb640fc2ed359e2cc756b0256bbce2126
SaracenRhue <77064702+SaracenRhue@users.noreply.github.com>
2022-09-20 12:10:55 +0200

    Update install_homebrew.sh

--- desktops/fedora/install_homebrew.sh
+++ desktops/fedora/install_homebrew.sh
@@ -4,7 +4,7 @@ sudo yum groupinstall 'Development Tools'
 sudo yum install procps-ng curl file git
 sudo yum install libxcrypt-compat # needed by Fedora 30 and up
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+yes '' | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 
 echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> ~/.bashrc
