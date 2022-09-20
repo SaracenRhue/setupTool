10e2a8ce6578917510288ea29332ac96c7d923e4
SaracenRhue <77064702+SaracenRhue@users.noreply.github.com>
2022-09-20 12:10:18 +0200

    Update mac.sh

--- desktops/mac.sh
+++ desktops/mac.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+yes '' | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> ~/.zshrc
 source ~/.zshrc
