5c72de34321271de558be026923581f3c3ca90a6
SaracenRhue <77064702+SaracenRhue@users.noreply.github.com>
2022-09-30 10:29:40 +0200

    Update install_nix.sh

--- desktops/fedora/install_nix.sh
+++ desktops/fedora/install_nix.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 curl -L https://nixos.org/nix/install | sh
 . ./home/$USER/.nix-profile/etc/profile.d/nix.sh
-echo "allowUnfree = true;" >> ~/.config/nixpkgs/config.nix
+echo "export NIXPKGS_ALLOW_UNFREE=1" >> ~/.bashrc
+echo "export NIXPKGS_ALLOW_UNFREE=1" >> ~/.zshrc
 
 nix-env -iA nixpkgs.brave
 nix-env -iA nixpkgs.vscode
