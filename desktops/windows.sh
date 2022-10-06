d4ae0e966c29edb13e59fac20116caa3500216cc
SaracenRhue <77064702+SaracenRhue@users.noreply.github.com>
2022-10-06 18:24:37 +0200

    edit

--- desktops/windows.sh
+++ desktops/windows.sh
@@ -1,5 +1,56 @@
-scoop install vscode brave firefox discord github nodejs python310 go rust pia-desktop vsc
-npm install -g typescript
-npm install -g sass
-npm install -g @angular/cli
-pip3 install selenium eel pyautogui pyyaml pygame Pillow
+#!/bin/bash
+
+# packages
+scoop bucket add java
+PKGS=(
+'python310'
+'nodejs'
+'go'
+'rust'
+'openjdk'
+'kotlin'
+)      
+for PKG in "${PKGS[@]}"; do
+    scoop install $PKG
+done
+
+# npm packages
+PKGS=(
+'typescript'
+'sass'
+'@angular/cli'
+)      
+for PKG in "${PKGS[@]}"; do
+    npm install -g $PKG
+done
+
+# python modules
+PKGS=(
+'selenium'
+'eel'
+'pyautogui'
+'pyyaml'
+'pygame'
+'Pillow'
+)      
+for PKG in "${PKGS[@]}"; do
+    pip3 install $PKG
+done
+
+# programs
+PKGS=(
+'vscode'
+'brave'
+'firefox'
+'discord'
+'github'
+'vlc'
+'pia-desktop'
+'wox'
+'bitwarden'
+'7zip'
+'winrar'
+)
+for PKG in "${PKGS[@]}"; do
+    scoop install $PKG
+done
