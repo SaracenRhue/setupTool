d999d3a5354083df516f2d499c7407f5ee84cee2
SaracenRhue <77064702+SaracenRhue@users.noreply.github.com>
2022-12-28 14:07:45 +0100

    u

---
+++ desktops/debian/install_nala.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo apt update && sudo apt upgrade -y
+sudo apt install -y wget
+echo "deb [arch=amd64,arm64,armhf] http://deb.volian.org/volian/ scar main" | sudo tee /etc/apt/sources.list.d/volian-archive-scar-unstable.list
+wget -qO - https://deb.volian.org/volian/scar.key | sudo tee /etc/apt/trusted.gpg.d/volian-archive-scar-unstable.gpg > /dev/null
+sudo apt update && sudo apt install nala -y
